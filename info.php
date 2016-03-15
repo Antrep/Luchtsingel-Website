9108f938a971833ab738edafc843c7857fa1dffe
Mufaesa <tfjvrijmoet@hotmail.com>
2016-03-15 14:38:55 +0100

    Add/Update files
    Inserted all files for timepicki. Also made timepicki sort of work but
    is not optimized yet.

--- info.php
+++ info.php
@@ -2,11 +2,11 @@
 
 ?>
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"
-        "http://www.w3.org/TR/html4/loose.dtd">
+    "http://www.w3.org/TR/html4/loose.dtd">
 <html>
 <head>
     <link href="css/infoStyle.css" rel="stylesheet" type="text/css">
-  <title></title>
+    <title></title>
 </head>
 <body>
 <div id="container">
--- live.php
+++ live.php
@@ -1,7 +1,39 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: Tom
- * Date: 15-3-16
- * Time: 11:56
- */
+?>
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"
+    "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+    <link rel="stylesheet" type="text/css" href="css/timepicki.css">
+    <link href="css/style.css" rel="stylesheet" type="text/css">
+    <title></title>
+</head>
+<body>
+
+<div id="container">
+    <div id="header">
+
+    </div>
+
+    <div id="content">
+        <div id="nav">
+        </div>
+        <div id="main">
+
+        </div>
+    </div>
+</div>
+<form>
+    <input type="text" name="timepicker" class="time_element"/>
+</form>
+
+</body>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
+<script src="js/loadPage.js"></script>
+<script src="js/timepicki.js"></script>
+<script>
+    $(document).ready(function(){
+        $(".time_element").timepicki();
+    });
+</script>
+</html>
