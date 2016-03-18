46922921869d6cbfd5a49d06a81ef35a8d1bb59f
guuse <ekkelenkampguus@gmail.com>
2016-03-18 10:36:27 +0100

    Added search test
    prototype voor de test functie

---
+++ test/searchTest.php
@@ -0,0 +1,69 @@
+<?php
+?>
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"
+    "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+    <link rel="stylesheet" type="text/css" href="../css/timepicki.css">
+    <link href="css/test.css" rel="stylesheet" type="text/css">
+    <title></title>
+</head>
+<body>
+
+
+<input type="text" id="search" placeholder="Kies een dag">
+
+<!--    <form>-->
+<!--        <input type="text" id="search" name="timepicker" class="time_element" style="width 300px; height: 40px; font-size: large"/>-->
+<!--    </form>-->
+
+<table id="table">
+    <tr>
+        <td>Maandag</td>
+        <td>07-03-2016</td>
+        <td><a href="maandag.php">Zoek uw video</a></td>
+
+    </tr>
+    <tr>
+        <td>Dinsdag</td>
+        <td>08-03-2016</td>
+        <td><a href="dinsdag.php">Zoek uw video</a></td>
+
+    </tr>
+    <tr>
+        <td>Woensdag</td>
+        <td>09-03-2016</td>
+        <td><a href="woensdag.php">Zoek uw video</a></td>
+
+    </tr>
+    <tr>
+        <td>Donderdag</td>
+        <td>10-03-2016</td>
+        <td><a href="donderdag.php">Zoek uw video</a></td>
+
+    </tr>
+    <tr>
+        <td>Vrijdag</td>
+        <td>11-03-2016</td>
+        <td><a href="vrijdag.php">Zoek uw video</a></td>
+
+    </tr>
+    <tr>
+        <td>Zaterdag</td>
+        <td>12-03-2016</td>
+        <td><a href="zaterdag.php">Zoek uw video</a></td>
+
+    </tr>
+    <tr>
+        <td>Zondag</td>
+        <td>13-03-2016</td>
+        <td><a href="zondag.php">Zoek uw video</a></td>
+
+    </tr>
+</table>
+
+
+</body>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
+<script src="js/search.js"></script>
+</html>
